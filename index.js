deca883933699c258fbebbdfbd7f9251cc29bc6a
RianVitor26 <rianvitorlhe@gmail.com>
2022-09-01 14:55:23 -0300

    feat: creating UI for home page

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const server = express();
 const port = process.env.PORT || 8080;
 const cors = require('cors');
+const path = require('path');
 
 const animes = require('./api.json')
 
+server.set('view engine', 'ejs');
+server.set('views', './views');
+
+server.use(express.static(path.join(__dirname, 'public')))
+
 server.use(cors())
+
+server.get('/', (req, res) => {
+    res.render('index')
+})
 
 server.get('/animeapi', (req, res) => {
     res.json(animes)
